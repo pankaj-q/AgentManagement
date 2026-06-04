bbd3f2859a8977d2d80dd77a35387d5a625f0168
Pankaj <codepankaj84@gmail.com>
2026-06-04 08:16:57 +0530

    Implementing Redis

---
+++ backend/config/redis.js
@@ -0,0 +1,8 @@
+const Redis = require('redis');
+
+const redis = new Redis(process.env.REDIS_URL);
+
+redis.on("connect", () => console.log("Redis connected"));
+redis.on("error", (err) => console.log("Redis error", err));
+
+module.exports = redis;
